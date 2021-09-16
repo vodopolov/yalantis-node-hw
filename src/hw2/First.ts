780d2015f63c59c46962108893f6d77d8239038e
Anton <anton.vodopolov@gmail.com>
2021-09-16 09:42:19 +0300

    First task was implemented

---
+++ src/hw2/First.ts
@@ -0,0 +1,30 @@
+
+type Item = { item: string, index: number }
+
+export async function runSequentially<T>(
+    arrayParam: T[],
+    callback: (item: T, index: number) => Promise<Item>
+) {
+    const result: Item[] = []
+    for (let i = 0; i < arrayParam.length; i++) {
+        result.push(await callback(arrayParam[i], i))
+    }
+    return result
+}
+
+async function main() {
+    const arrayForTest: Array<string> = ["one", "two", "three"]
+
+    const results = await runSequentially(arrayForTest, (item, index) => {
+        return new Promise((resolve) => {
+            setTimeout(resolve, 1000,
+                {
+                    item,
+                    index,
+                })
+        })
+    })
+    console.log(JSON.stringify(results))
+}
+
+main()
